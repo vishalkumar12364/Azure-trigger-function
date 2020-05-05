fe44203941391c3994813cc8cac55b1d54be165e
Vishal Kumar <kumarvishal12364@gmail.com>
2020-05-05 13:59:47 +0530

    Add project files.

---
+++ Azure trigger function/TimerFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Azure_trigger_function
+{
+    public static class TimerFunction
+    {
+        [FunctionName("TimerFunction")]
+        public static void Run([TimerTrigger("0,10 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
